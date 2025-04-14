61508deea465cae19774054f9f7e24e1c0ca00aa
Lewis-Walter-K <104240426@student.vgu.edu.vn>
2025-04-15 01:58:48 +0700

    realign text & centering

--- src/components/HomePageComponents/InforSection.js
+++ src/components/HomePageComponents/InforSection.js
@@ -19,7 +19,7 @@ function InforSection() {
               VGU Career Fair and Industry Exploration Day 2025 is a flagship event to foster collaboration, 
               innovation, and career exploration. CFIED25 aims to:
               </p>
-              <ul className="px-14 lg:text-xl text-justify  mt-4 list-disc list-outside space-y-2 text-gray-700 tracking-normal italic">
+              <ul className="px-14 lg:text-xl text-left mt-4 list-disc list-outside space-y-2 text-gray-700 tracking-normal italic">
                 <li>Connect VGU students with industry partners for careers and internships. </li>
                 <li>Provide a platform for companies to showcase technologies and opportunities.</li>
                 <li>Build and strengthen VGU-industry partnerships for collaboration and knowledge exchange.</li>
@@ -29,7 +29,7 @@ function InforSection() {
 
             {/* Right Section */}
             <div className="lg:w-1/2 text-right">
-              <h2 className="text-yellow-500 text-5xl lg:text-7xl font-bold leading-tight px-5" style={{fontFamily:"IntroRust"}}> {/* Text with gradient and Intro Rust font */}
+              <h2 className="text-yellow-500 text-5xl lg:text-7xl mt-5 font-bold leading-tight px-5" style={{fontFamily:"IntroRust"}}> {/* Text with gradient and Intro Rust font */}
                 INFORMATION ABOUT THE EVENT
               </h2>
               <div className="w-3/5 lg:w-auto px-5 lg:pl-0 lg:mt-6 text-2xl lg:text-3xl text-right place-self-end lg:place-self-start">
@@ -38,17 +38,25 @@ function InforSection() {
             </div>
           </div>
         </section>
-        <div className="lg:pl-36 float-left pl-10 lg:mt-10">
+        <div className="lg:pl-36 float-left text-left pl-16 mt-5 lg:mt-10">
           <strong className="text-3xl">Contact Information</strong>
           <p className="text-lg">Mrs. Vo Phan Hoang Trang - Tel. - Email:</p>
         </div>
         <div>
-        <div className="w-11/12 mt-20 lg:w-8/12 h-2 lg:h-4 lg:mt-4 lg:mb-8 lg:mb-5 mx-auto lg:float-right" style={{backgroundColor:"#145f48"}}></div> {/* Teal footer */}
-          <div className="float-right flex gap-6 mr-12 h-10 mt-6 lg:mt-0">
-              <img src={email}></img>
-              <img src={facebook}></img>
-              <img src={location}></img>
-              <img src={www}></img>
+        <div className="w-11/12 lg:w-8/12 h-2 lg:h-4 lg:mt-4 lg:mb-8 lg:mb-5 mx-auto lg:float-right" style={{backgroundColor:"#145f48"}}></div> {/* Teal footer */}
+          <div className="float-right flex gap-6 mr-20 lg:mr-12 h-12 mt-6 lg:mt-0">
+            <div className="icon">
+              <img src={www} className="icon-img"></img>
+            </div>
+            <div className="icon">
+              <img src={facebook} className="icon-img"></img>
+            </div>
+            <div className="icon">
+              <img src={email} className="icon-img"></img>
+            </div>
+            <div className="icon">
+              <img src={location} className="icon-img"></img>
+            </div>
           </div>
         </div>
       </div>
