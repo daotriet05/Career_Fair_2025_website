80c9870890acecad9b12feba627853b508285dfd
Triet Dao <daohoangminhtriet@gmail.com>
2025-04-14 19:30:38 +0700

    mostly finished 3 slides for Job list, Workshop, and Recap, but there are several things to complete.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -21,7 +21,7 @@ function HomePage() {
                 <AgendaSection />
                 <JobsSection />
                 <WorkshopSection />
-                <SponsorSection />
+                <RecapSection />
                 <CoreteamSection />
             </div>
         </div>
