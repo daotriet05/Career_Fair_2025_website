0a1e80e8f22186a38609cd477232306d3e433a6f
Triet Dao <daohoangminhtriet@gmail.com>
2025-04-16 22:21:13 +0700

    added Dashboard for accounts, modified Homepage, HeaderBar components

---
+++ src/components/DashboardComponents/BoothData.js
@@ -0,0 +1,19 @@
+// boothData.js
+export const boothsStatus = {
+    indefo: true,
+    fpt: true,
+    wanek: true,
+    lego: true,
+    mitek: true,
+    adnovum: true,
+    netcompany: true,
+    ang: true,
+    bosch: true,
+    spx: true,
+    // 15 more booths with false value
+  };
+  
+  export const user = {
+    name: "DAO HOANG MINH TRIET"
+  };
+  
