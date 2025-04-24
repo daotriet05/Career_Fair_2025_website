82e9a8761b33579708a53ea1646303776fb02d10
Triet Dao <daohoangminhtriet@gmail.com>
2025-04-24 23:05:07 +0700

    convert png to webp images for sponsor section

--- src/components/HomePageComponents/SponserNowSection.js
+++ src/components/HomePageComponents/SponserNowSection.js
@@ -1,47 +1,50 @@
 import React, {useState} from 'react';
-import img1 from "../../logo/Wanek Furniture.png";
-import img2 from "../../logo/LEGO.png";
-import img3 from "../../logo/Indefol.png";
-import img4 from "../../logo/Ziehl Abegg.png";
-import img5 from "../../logo/Bosch.png";
-import img6 from "../../logo/Endress Hauser.png";
-import img7 from "../../logo/Techcombank.png";
-import img8 from "../../logo/Baker Hughes.png";
-import img9 from "../../logo/fischer.png";
-import img10 from "../../logo/MAC ZT.png";
-import img11 from "../../logo/NTPM.png";
-import img12 from "../../logo/Renesas.png";
-import img13 from "../../logo/Mitek.png";
-import img14 from "../../logo/Kyungbang.png";
-import img15 from "../../logo/Netcompany.png";
-import img16 from "../../logo/FPT Software.png";
-import img17 from "../../logo/Nextern.png";
-import img18 from "../../logo/TTI.png";
-import img19 from "../../logo/Shopee Express.png";
-import img20 from "../../logo/Adnovum.png";
 
-const images = [
-    {image: img1, title: "Wanek Furniture", url: "https://www.facebook.com/VGU.CFIED" },  
-    {image: img2, title: "LEGO", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img3, title: "Indefol", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img4, title: "Ziehl Abegg", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img5, title: "Bosch", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img6, title: "Endress Hauser", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img7, title: "Techcombank", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img8, title: "Baker Hughes", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img9, title: "fischer", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img10, title: "MAC ZT", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img11, title: "NTPM", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img12, title: "Renesas", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img13, title: "Mitek", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img14, title: "Kyungbang", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img15, title: "Netcompany", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img16, title: "FPT Software", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img17, title: "Nextern", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img18, title: "TTI", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img19, title: "Shopee Express", url: "https://www.facebook.com/VGU.CFIED" },
-    {image: img20, title: "Adnovum", url: "https://www.facebook.com/VGU.CFIED" },
-]
+import Adnovum from "../../images/logo_2025/Adnovum.webp";
+import SAP from "../../images/logo_2025/SAP.webp";
+import Bosch from "../../images/logo_2025/Bosch.webp";
+import EndressHauser from "../../images/logo_2025/Endress Hauser.webp";
+import Fischer from "../../images/logo_2025/fischer.webp";
+import FPTSoftware from "../../images/logo_2025/FPT Software.webp";
+import Indefol from "../../images/logo_2025/Indefol.webp";
+import Kyungbang from "../../images/logo_2025/Kyungbang.webp";
+import LEGO from "../../images/logo_2025/LEGO.webp";
+import MACZT from "../../images/logo_2025/MAC ZT.webp";
+import Mitek from "../../images/logo_2025/mitek.webp";
+import Netcompany from "../../images/logo_2025/Netcompany.webp";
+import Nextern from "../../images/logo_2025/nextern.webp";
+import Renesas from "../../images/logo_2025/Renesas.webp";
+import ShopeeExpress from "../../images/logo_2025/Shopee Express.webp";
+import TTI from "../../images/logo_2025/TTI.webp";
+import Wanek from "../../images/logo_2025/wanek.webp";
+import ZiehlAbegg from "../../images/logo_2025/Ziehl Abegg.webp";
+
+const logoOrder = [
+    { image: Wanek, alt: "Wanek Furniture", url: "https://www.facebook.com/VGU.CFIED" },
+    { image: LEGO, alt: "LEGO" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Indefol, alt: "Indefol" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: ZiehlAbegg, alt: "Ziehl Abegg" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Bosch, alt: "Bosch" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: EndressHauser, alt: "Endress Hauser" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: SAP, alt: "SAP" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Fischer, alt: "Fischer" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: MACZT, alt: "MAC ZT" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Renesas, alt: "Renesas" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Mitek, alt: "Mitek" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Kyungbang, alt: "Kyungbang" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Netcompany, alt: "Netcompany" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: FPTSoftware, alt: "FPT Software" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Nextern, alt: "Nextern" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: TTI, alt: "TTI" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: ShopeeExpress, alt: "Shopee Express" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Adnovum, alt: "Adnovum" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Wanek, alt: "Wanek Furniture", url: "https://www.facebook.com/VGU.CFIED" },
+    { image: LEGO, alt: "LEGO" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: Indefol, alt: "Indefol" , url: "https://www.facebook.com/VGU.CFIED"},
+    { image: ZiehlAbegg, alt: "Ziehl Abegg" , url: "https://www.facebook.com/VGU.CFIED"},
+
+  ];
+
 
 const GnSCard = ({ image, title }) => {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -62,7 +65,7 @@ const GnSCard = ({ image, title }) => {
     )
 }
 
-const BronzeCard = ({ image, title }) => {
+const ExhiCard = ({ image, title }) => {
     const [isLoaded, setIsLoaded] = useState(false);
     return(
         <div className="w-[250px] sm:w-[200px] flex justify-self-center">
@@ -98,7 +101,7 @@ function SponserNowSection(){
             <div className="w-full" style={{border:"3px solid black"}}></div>
             {/* Gold Sponser (2 items) */}
             <div className="py-16 grid grid-cols-1 sm:grid-cols-2 gap-x-[390px] gap-y-[70px]">
-                {images.slice(0, 2).map((image, idx) => (
+                {logoOrder.slice(0, 2).map((image, idx) => (
                     <a 
                     key={idx} 
                     href={image.url}
@@ -114,7 +117,7 @@ function SponserNowSection(){
             <div className="w-full" style={{border:"3px solid black"}}></div>
             {/* Silver Sponser (2 items) */}
             <div className="py-16 grid grid-cols-1 sm:grid-cols-2 gap-x-[390px] gap-y-[70px]">
-                {images.slice(2, 4).map((image, idx) => (
+                {logoOrder.slice(2, 4).map((image, idx) => (
                     <a                     
                     key={idx} 
                     href={image.url}
@@ -126,44 +129,57 @@ function SponserNowSection(){
                 ))}
             </div>
 
-            <h1 className="text-xl p-2 font-bold">Bronze Sponsers</h1>
+            <h1 className="text-xl p-2 font-bold">Exhibitors</h1>
             <div className="w-full" style={{border:"3px solid black"}}></div>
-            {/* Bronze Sponser lv1 (5 items) */}
-            <div className="py-16 grid grid-cols-1 sm:grid-cols-5 gap-x-[100px] gap-y-[70px]">
-                {images.slice(4, 9).map((image, idx) => (
+            {/* Exhibitors lv1 (5 items) */}
+            <div className="py-16 grid grid-cols-1 sm:grid-cols-4 gap-x-[100px] gap-y-[70px]">
+                {logoOrder.slice(4, 8).map((image, idx) => (
                     <a 
                     key={idx} 
                     href={image.url}
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="transition hover:scale-110">
-                        <BronzeCard key={idx} {...image} />
+                        <ExhiCard key={idx} {...image} />
                     </a>
                 ))}
             </div>
-             {/* Bronze Sponser lv2 (4 items / rows) */}
+             {/* Exhibitors Sponser lv2 (4 items / rows) */}
              <div className="grid grid-rows-2 grid-cols-1 sm:grid-cols-4 gap-x-[100px] gap-y-[70px]">
-                {images.slice(9, 17).map((image, idx) => (
+                {logoOrder.slice(8, 16).map((image, idx) => (
                     <a 
                     key={idx} 
                     href={image.url}
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="transition hover:scale-110">
-                        <BronzeCard key={idx} {...image} />
+                        <ExhiCard key={idx} {...image} />
                     </a>
                 ))}
             </div>
-             {/* Bronze Sponser lv3 (3 items / rows) */}
+             {/* Exhibitors lv3 (3 items / rows) */}
              <div className="py-[70px] grid grid-cols-1 sm:grid-cols-3 gap-x-[100px] gap-y-[70px]">
-                {images.slice(17, 21).map((image, idx) => (
+                {logoOrder.slice(16, 19).map((image, idx) => (
                     <a
                     key={idx} 
                     href={image.url}
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="transition hover:scale-110">
-                        <BronzeCard key={idx} {...image} />                        
+                        <ExhiCard key={idx} {...image} />                        
+                    </a>
+                ))}
+            </div>
+            {/* Exhibitors lv4 (3 items / rows) */}
+            <div className="py-[70px] grid grid-cols-1 sm:grid-cols-3 gap-x-[100px] gap-y-[70px]">
+                {logoOrder.slice(19, 22).map((image, idx) => (
+                    <a
+                    key={idx} 
+                    href={image.url}
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="transition hover:scale-110">
+                        <ExhiCard key={idx} {...image} />                        
                     </a>
                 ))}
             </div>
